1bcac87dcac72e68b20e35fb1a6fcc126b071cff
unknown <rashadbayar@gmail.com>
2020-04-01 10:12:40 -0700

    update

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
 
-
 $(document).ready(function () {
+        
+console.log("works")
+            
+        
 
-
-      $("#schedule").timeSchedule('addRow', timeline, {
-        
-          title:'Title Area',
-        
-          schedule:[
-        
-            {
-        
-              start:'09:00',
-        
-              end:'12:00',
-        
-              text:'Text Area',
-        
-              data: {}
-    
-            }
-        
-          ]
-        
-        });
-        
+  
 
 
 
 
 
 
-}); 
+
+});
 
