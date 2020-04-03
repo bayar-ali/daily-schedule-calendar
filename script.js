1b2829485c3b39d95b36761a4fd3d0efe5cd3b58
unknown <rashadbayar@gmail.com>
2020-04-03 16:16:28 -0700

    update

--- script.js
+++ script.js
@@ -1,56 +1,93 @@
-var todayDate = document.querySelector("#currentDay")
-
-        
-var textAreaEl = document.querySelector("#textArea"); 
-var saveButton = document.querySelector("#savebutton");
-
-
-// the date 
-
-var currentDate = new Date();
-var month = currentDate.getMonth();
-var day = currentDate.getDate(); 
-var year = currentDate.getFullYear(); 
-var fullDate = month + "/" + day + "/" + year;
-document.write(fullDate); 
-//
-
-
-saveButton.onclick = function() {
-    const key = saveButton.value;
-
-    const value = textAreaEl.value;
-
-console.log(key)
-console.log(value)
-
-    if (key && value) {
-        localStorage.setItem(key, value); 
-        location.reload(); 
-    }
-};
-
-for (let i =0; i < localStorage.length; i++) {
-
-    const key = localStorage.key(i);
-
-    const value = localStorage.getItem(key); 
-
-    textAreaEl.innerHTML += `${key}; ${value}<br />`; 
-
-}
-
-
-
-    
-
-
-
-
-
-
-
-
  
+    var todayDate = document.querySelector("#currentDay")
+
+
+
+    var textAreaEl = document.querySelector("#textArea");
+    var saveButton = document.querySelector("#button");
+    var saveButton1 = document.querySelector("#button1")
+
+
+    // the date 
+
+    var currentDate = new Date();
+    var month = currentDate.getMonth();
+    var day = currentDate.getDate();
+    var year = currentDate.getFullYear();
+    var fullDate = month + "/" + day + "/" + year;
+    document.write(fullDate);
+    //
+
+
+    button1.onclick = function () {
+        const key = button1.value;
+
+        const value = textAreaEl.value;
+
+        console.log(key)
+        console.log(value)
+
+        if (key && value) {
+            localStorage.setItem(key, value);
+            location.reload();
+        }
+    };
+    
+    saveButton.onclick = function () {
+        const key = saveButton.value;
+
+        const value = textAreaEl.value;
+
+        console.log(key)
+        console.log(value)
+
+        if (key && value) {
+            localStorage.setItem(key, value);
+            location.reload();
+        }
+    };
+
+    for (let i = 0; i < localStorage.length; i++) {
+
+        const key = localStorage.key(i);
+
+        const value = localStorage.getItem(key);
+
+        textAreaEl.innerHTML += `${key}; ${value}<br />`;
+
+    } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
